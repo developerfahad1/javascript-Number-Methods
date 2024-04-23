9599277c48b48a363fd75f56b1563d819ea71c51
developerfahad1 <fahadgraphic.pk>
2024-04-23 11:40:50 +0500

    app.js and index.html file created

---
+++ app.js
@@ -0,0 +1,17 @@
+// Number Methods
+// round
+// ceil 
+// floor
+// roandom
+
+
+
+
+
+// const num = 2.54;
+
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+
+console.log(Math.random(10));
